cbc36ddde9bcbe54a97f4b528afbaab8a4449c53
Juan Rojas <jrestrad20@gmail.com>
2024-07-26 08:33:09 -0500

    Primeros pasos

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,31 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main (){
-  runApp(MaterialApp());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
 }
 
